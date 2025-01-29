992b7e0fc571e4ed3da0f328fb0e61a851b43f03
Thomas <thomas.burg@gmail.com>
2025-01-29 23:51:05 +0100

    Replace distutils not anymore available in python 3.12

--- cola/swig-python-setup.py
+++ cola/swig-python-setup.py
@@ -4,38 +4,62 @@
 setup.py file for SWIG example
 """
 
-from distutils.core import setup, Extension
-from distutils import util
-from distutils import sysconfig
+from setuptools import setup, Extension
+import platform
+import sys
 import os
+import sysconfig
 
-platform = util.get_platform()
+def get_platform_settings():
+    if platform.system().lower().startswith('linux'):
+        # Remove '-Wstrict-prototypes' flag if present
+        try:
+            cfg_vars = sysconfig.get_config_vars()
+            if "OPT" in cfg_vars:
+                cfg_vars["OPT"] = cfg_vars["OPT"].replace("-Wstrict-prototypes", "")
+        except Exception:
+            pass
 
-# special settings for Linux
-if platform.startswith("linux"):
-    # option '-Wstrict-prototypes' is enabled by default and gives a warning on Linux
-    if "OPT" in sysconfig.get_config_vars().keys():
-        opt = sysconfig.get_config_vars().get("OPT")
-        if type(opt) == str:
-            opt = opt.replace("-Wstrict-prototypes", "")
-            (sysconfig.get_config_vars())["OPT"] = opt
+        # Set compiler environment variables
+        os.environ['CC'] = 'gcc'
+        os.environ['CXX'] = 'g++'
 
-    os.environ['CC'] = 'gcc'
-    os.environ['CXX'] = 'g++'
+adaptagrams_module = Extension(
+    '_adaptagrams',
+    sources=['adaptagrams_wrap.cxx'],
+    extra_compile_args=[
+        '-DUSE_ASSERT_EXCEPTIONS',
+        '-DSWIG_PYTHON_SILENT_MEMLEAK',
+        '-std=gnu++11',
+#        '-stdlib=libc++'
+    ],
+    include_dirs=['.'],
+    extra_link_args=[
+        'libcola/.libs/libcola.a',
+        'libtopology/.libs/libtopology.a',
+        'libavoid/.libs/libavoid.a',
+        'libvpsc/.libs/libvpsc.a',
+        'libdialect/.libs/libdialect.a'
+    ],
+)
 
-adaptagrams_module = Extension('_adaptagrams',
-                               sources=['adaptagrams_wrap.cxx'],
-			                   extra_compile_args=['-DUSE_ASSERT_EXCEPTIONS','-DSWIG_PYTHON_SILENT_MEMLEAK','-std=gnu++11',],
-                               # use this line for SWIG 2.0.12 to silence some harmless warnings 
-                               #extra_compile_args=['-DUSE_ASSERT_EXCEPTIONS','-DSWIG_PYTHON_SILENT_MEMLEAK','-Wno-uninitialized'],
-			                   include_dirs=['.'],
-			                   extra_link_args=['libcola/.libs/libcola.a','libtopology/.libs/libtopology.a', 'libavoid/.libs/libavoid.a','libvpsc/.libs/libvpsc.a','libdialect/.libs/libdialect.a'],
-                              )
+if __name__ == '__main__':
+    # Apply platform-specific settings
+    get_platform_settings()
 
-setup (name = 'adaptagrams',
-       version = '0.1',
-       author      = "MArVL",
-       description = """Adaptagrams libraries""",
-       ext_modules = [adaptagrams_module],
-       py_modules = ["adaptagrams"],
-      )
+    setup(
+        name='adaptagrams',
+        version='0.1',
+        author='MArVL',
+        description='Adaptagrams libraries',
+        ext_modules=[adaptagrams_module],
+        py_modules=['adaptagrams'],
+        python_requires='>=3.6',  # Add minimum Python version requirement
+        # Add additional metadata
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Programming Language :: Python :: 3',
+            'Programming Language :: C++',
+        ],
+    )
